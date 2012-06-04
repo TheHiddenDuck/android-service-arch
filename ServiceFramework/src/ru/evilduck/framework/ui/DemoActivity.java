fa923d36d5a4022c1cf3f6a5f7fc9c1cd3049565
Alexander Osmanov <alex.osmanov.88@gmail.com>
2012-06-04 22:43:51 +0400

    minor fixes

--- ServiceFramework/src/ru/evilduck/framework/ui/DemoActivity.java
+++ ServiceFramework/src/ru/evilduck/framework/ui/DemoActivity.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright (C) 2012 Alexander Osmanov (http://www.perfectearapp.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package ru.evilduck.framework.ui;
 
+import ru.evilduck.framework.R;
 import ru.evilduck.framework.SFBaseActivity;
 import ru.evilduck.framework.handlers.impl.TestActionHandler;
 import android.app.Dialog;
@@ -13,8 +30,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
-
-import ru.evilduck.framework.R;
 
 public class DemoActivity extends SFBaseActivity {
 
